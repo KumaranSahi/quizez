01901374fd1bd5c84acc3ed1462715d09a4bb0b5
Muthu Kumaran <61352185+KumaranSahi@users.noreply.github.com>
2021-05-21 01:21:41 +0530

    feature-added create quiz and leaderboard component

---
+++ src/Components/CreateQuizAndLeaderBoard/CreateQuizAndLeaderBoard.tsx
@@ -0,0 +1,56 @@
+import classes from './CreateQuizAndLeaderBoard.module.css'
+import {Table,TableBody,TableCell,TableContainer,TableHead,TableRow,Paper,Button} from '@material-ui/core';
+import {useHistory} from 'react-router-dom'
+import {useQuiz} from '../../Store/QuizContext/QuizContext'
+
+export const CreateQuizAndLeaderBoard=()=>{
+    const {leaderBoard}=useQuiz()
+    const {push}=useHistory()
+    return(
+        <div className={classes["create-quiz-personal-scores"]}>
+            <Button 
+                variant="outlined"  
+                color="primary"
+                fullWidth
+                size="large"
+                onClick={()=>push("/create-quiz")}
+            >
+                Create Quiz
+            </Button>
+            <hr/>
+            <Button 
+                variant="contained"  
+                color="primary"
+                fullWidth
+                size="large"
+                onClick={()=>push("/my-quizes")}
+            >
+                My Quizes
+            </Button>    
+            <h1>
+                Top 10 Leader Board
+            </h1>
+            <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="customized table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell className={classes["table-head-cell"]}>Player</TableCell>
+                        <TableCell className={classes["table-head-cell"]} align="right">Quiz</TableCell>
+                        <TableCell className={classes["table-head-cell"]} align="right">Score</TableCell>
+                    </TableRow>
+                </TableHead>
+            <TableBody>
+            {leaderBoard.map(({id,score,userName,quizName})=>(
+                <TableRow key={id} className={classes["tablebody-row"]}>
+                    <TableCell>{userName}</TableCell>
+                    <TableCell align="right">{quizName}</TableCell>
+                    <TableCell align="right">{score}</TableCell>
+                    
+                </TableRow>
+            ))}
+            </TableBody>
+        </Table>
+        </TableContainer>
+    </div>
+    )
+}
--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,3 +1,5 @@
 export {Navbar} from './Navbar/Navbar'
 export {infoToast,successToast,warningToast} from './Toast/Toast'
 export {Spinner} from './Spinner/Spinner'
+export {MobileNavbar} from './MobileNavbar/MobileNavbar'
+export {CreateQuizAndLeaderBoard} from './CreateQuizAndLeaderBoard/CreateQuizAndLeaderBoard'

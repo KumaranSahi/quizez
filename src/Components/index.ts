9066c516d52a6649bc74c9a280ac697c7348dbc9
Muthu Kumaran <61352185+KumaranSahi@users.noreply.github.com>
2021-06-24 02:29:10 +0530

    fix-component folder naming fix

--- src/Components/index.ts
+++
@@ -1,5 +0,0 @@
-export { Navbar } from "./Navbar/Navbar";
-export { infoToast, successToast, warningToast } from "./Toast/Toast";
-export { Spinner } from "./Spinner/Spinner";
-export { MobileNavbar } from "./MobileNavbar/MobileNavbar";
-export { CreateQuizAndLeaderBoard } from "./CreateQuizAndLeaderBoard/CreateQuizAndLeaderBoard";

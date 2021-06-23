c44bbb7f64b0ec51072c794f27bf6077b95c34db
Muthu Kumaran <61352185+KumaranSahi@users.noreply.github.com>
2021-06-24 02:01:13 +0530

    fix-store folder renaming

--- src/Store/index.ts
+++
@@ -1,6 +0,0 @@
-export { useAuth, AuthContextProvider } from "./AuthContext/AuthContext";
-export { useQuiz, QuizContextProvider } from "./QuizContext/QuizContext";
-export {
-  usePlayQuiz,
-  PlayQuizContextProvider,
-} from "./PlayQuizContext/PlayQuizContext";

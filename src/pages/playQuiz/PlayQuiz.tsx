6f505f2c01e8ac56fc8e00f7d56732295fbc2b57
Muthu Kumaran <61352185+KumaranSahi@users.noreply.github.com>
2021-07-16 01:07:58 +0530

    feature- play quiz page migrated

--- src/pages/playQuiz/PlayQuiz.tsx
+++ src/pages/playQuiz/PlayQuiz.tsx
@@ -1,13 +1,20 @@
 import { useEffect } from "react";
-import classes from "./PlayQuiz.module.css";
 import { useQuiz, usePlayQuiz } from "../../store";
 import { Prompt, useHistory } from "react-router-dom";
 import { Question } from "./question/Question";
+import {
+  HStack,
+  useColorModeValue,
+  useMediaQuery,
+  Box,
+} from "@chakra-ui/react";
 
 export const PlayQuiz = () => {
   const { playQuizMode, currentQuestion } = usePlayQuiz();
   const { currentQuiz } = useQuiz();
   const { push } = useHistory();
+
+  const [isLargerThan700] = useMediaQuery("(min-width: 700px)");
 
   useEffect(() => {
     window.addEventListener("beforeunload", alertUser);
@@ -32,8 +39,20 @@ export const PlayQuiz = () => {
         when={playQuizMode}
         message="The game will be lost, Are you sure you want to navigate away?"
       />
-      <div className={classes["play-quiz-page"]}>
-        <div className={classes["play-quiz-container"]}>
+      <HStack
+        width="100%"
+        height="100%"
+        justifyContent="center"
+        alignItems="flex-start"
+        bg={useColorModeValue("lightgray", "gray.800")}
+      >
+        <Box
+          width={isLargerThan700 ? "40rem" : "100%"}
+          minHeight="85vh"
+          textAlign="center"
+          padding="1rem"
+          bg={useColorModeValue("white", "gray.900")}
+        >
           {currentQuestion && (
             <Question
               id={currentQuestion.id}
@@ -51,8 +70,8 @@ export const PlayQuiz = () => {
               }
             />
           )}
-        </div>
-      </div>
+        </Box>
+      </HStack>
     </>
   );
 };

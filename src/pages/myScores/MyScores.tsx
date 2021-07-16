26af0cc973eeae12e3f8b57316784907821e8722
Muthu Kumaran <61352185+KumaranSahi@users.noreply.github.com>
2021-07-16 19:03:03 +0530

    feature- myScores migrated to chakraUI

--- src/pages/myScores/MyScores.tsx
+++ src/pages/myScores/MyScores.tsx
@@ -1,15 +1,18 @@
-import classes from "./MyScores.module.css";
 import { useQuiz } from "../../store";
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
+  useMediaQuery,
   Button,
-} from "@material-ui/core";
+  HStack,
+  useColorModeValue,
+  Box,
+  Heading,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+} from "@chakra-ui/react";
 import { useHistory } from "react-router-dom";
 
 export const MyScores = () => {
@@ -17,50 +20,55 @@ export const MyScores = () => {
   const { push } = useHistory();
   const { getQuiz, setQuizLoading, dispatch: quizDispatch } = useQuiz();
 
+  const [isLargerThan700] = useMediaQuery("(min-width: 700px)");
+
   const playQuiz = (quizId: string) => {
     getQuiz(quizId, quizDispatch, setQuizLoading, false);
     push("/rules");
   };
 
   return (
-    <div className={classes["my-scores-page"]}>
-      <div className={classes["my-scores-container"]}>
-        <h1>My Top 10</h1>
-        <TableContainer component={Paper}>
-          <Table className={classes.table} aria-label="customized table">
-            <TableHead>
-              <TableRow>
-                <TableCell className={classes["table-head-cell"]}>
-                  Quiz
-                </TableCell>
-                <TableCell className={classes["table-head-cell"]} align="right">
-                  Score
-                </TableCell>
-                <TableCell className={classes["table-head-cell"]} align="right">
-                  Play Again
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {myLeaderBoard.map(({ id, score, quizName, quizId }) => (
-                <TableRow key={id} className={classes["tablebody-row"]}>
-                  <TableCell>{quizName}</TableCell>
-                  <TableCell align="right">{score}</TableCell>
-                  <TableCell align="right">
-                    <Button
-                      color="primary"
-                      variant="contained"
-                      onClick={() => playQuiz(quizId!)}
-                    >
-                      Play Again
-                    </Button>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </div>
-    </div>
+    <HStack
+      width="100%"
+      height="100%"
+      justifyContent="center"
+      bg={useColorModeValue("lightgray", "gray.800")}
+    >
+      <Box
+        width={isLargerThan700 ? "40rem" : "100%"}
+        minHeight="90vh"
+        textAlign="center"
+        padding="1rem"
+        bg={useColorModeValue("white", "gray.900")}
+      >
+        <Heading>My Top 10</Heading>
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th>Quiz</Th>
+              <Th align="right">Score</Th>
+              <Th align="right">Play Again</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {myLeaderBoard.map(({ id, score, quizName, quizId }) => (
+              <Tr key={id}>
+                <Td>{quizName}</Td>
+                <Td>{score}</Td>
+                <Td>
+                  <Button
+                    color="teal"
+                    variant="solid"
+                    onClick={() => playQuiz(quizId!)}
+                  >
+                    Play Again
+                  </Button>
+                </Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </Box>
+    </HStack>
   );
 };

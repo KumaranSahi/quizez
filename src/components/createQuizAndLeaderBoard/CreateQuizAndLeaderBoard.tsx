660a08f8383f0cc2b4fa2d0ffeba71c4827477f0
Muthu Kumaran <61352185+KumaranSahi@users.noreply.github.com>
2021-07-17 15:29:13 +0530

    feature- migrated leaderboard to chakraUI

--- src/components/createQuizAndLeaderBoard/CreateQuizAndLeaderBoard.tsx
+++ src/components/createQuizAndLeaderBoard/CreateQuizAndLeaderBoard.tsx
@@ -1,14 +1,14 @@
-import classes from "./CreateQuizAndLeaderBoard.module.css";
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
   Button,
-} from "@material-ui/core";
+  VStack,
+  Heading,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+} from "@chakra-ui/react";
 import { useHistory } from "react-router-dom";
 import { useQuiz, useAuth } from "../../store";
 
@@ -16,14 +16,15 @@ export const CreateQuizAndLeaderBoard = () => {
   const { leaderBoard } = useQuiz();
   const { push } = useHistory();
   const { isAdmin } = useAuth();
+
   return (
-    <div className={classes["create-quiz-personal-scores"]}>
+    <VStack width="100%" height="100%">
       {isAdmin && (
         <Button
-          variant="outlined"
-          color="primary"
-          fullWidth
-          size="large"
+          variant="ghost"
+          color="teal"
+          width="100%"
+          size="lg"
           onClick={() => push("/create-quiz")}
         >
           Create Quiz
@@ -32,42 +33,34 @@ export const CreateQuizAndLeaderBoard = () => {
       <hr />
       {isAdmin && (
         <Button
-          variant="contained"
-          color="primary"
-          fullWidth
-          size="large"
+          variant="solid"
+          color="teal"
+          width="100%"
+          size="lg"
           onClick={() => push("/my-quizes")}
         >
           My Quizes
         </Button>
       )}
-      <h1>Top 10 Leader Board</h1>
-      <TableContainer component={Paper}>
-        <Table className={classes.table} aria-label="customized table">
-          <TableHead>
-            <TableRow>
-              <TableCell className={classes["table-head-cell"]}>
-                Player
-              </TableCell>
-              <TableCell className={classes["table-head-cell"]} align="right">
-                Quiz
-              </TableCell>
-              <TableCell className={classes["table-head-cell"]} align="right">
-                Score
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {leaderBoard.map(({ id, score, userName, quizName }) => (
-              <TableRow key={id} className={classes["tablebody-row"]}>
-                <TableCell>{userName}</TableCell>
-                <TableCell align="right">{quizName}</TableCell>
-                <TableCell align="right">{score}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </div>
+      <Heading>Top 10 Leader Board</Heading>
+      <Table aria-label="customized table">
+        <Thead>
+          <Tr>
+            <Th>Player</Th>
+            <Th>Quiz</Th>
+            <Th>Score</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {leaderBoard.map(({ id, score, userName, quizName }) => (
+            <Tr key={id}>
+              <Td>{userName}</Td>
+              <Td>{quizName}</Td>
+              <Td>{score}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </VStack>
   );
 };
--- src/pages/createQuizAndLeaderBoardPage/CreateQuizAndLeaderBoardPage.tsx
+++ src/pages/createQuizAndLeaderBoardPage/CreateQuizAndLeaderBoardPage.tsx
@@ -1,12 +1,30 @@
-import classes from "./CreateQuizAndLeaderBoard.module.css";
 import { CreateQuizAndLeaderBoard } from "../../components";
+import {
+  useMediaQuery,
+  HStack,
+  useColorModeValue,
+  Box,
+} from "@chakra-ui/react";
 
 export const CreateQuizAndLeaderBoardPage = () => {
+  const [isLargerThan700] = useMediaQuery("(min-width: 700px)");
+
   return (
-    <div className={classes["create-quiz-leaderboard-page"]}>
-      <div className={classes["create-quiz-leaderboard-container"]}>
+    <HStack
+      width="100%"
+      height="100%"
+      justifyContent="center"
+      bg={useColorModeValue("lightgray", "gray.800")}
+    >
+      <Box
+        width={isLargerThan700 ? "40rem" : "100%"}
+        minHeight="90vh"
+        textAlign="center"
+        padding="1rem"
+        bg={useColorModeValue("white", "gray.900")}
+      >
         <CreateQuizAndLeaderBoard />
-      </div>
-    </div>
+      </Box>
+    </HStack>
   );
 };
